2658748f0bba998182bd0991aa5b22216986121e
Nong Hoang Tu <dmknght@parrotsec.org>
2021-09-28 21:05:57 +0700

    - Add set cookie - Value of proxy now is address only instead of dict - Value of user-agent (ua) now is value only instead of dict

--- vjyscan/cores/http_session.py
+++ vjyscan/cores/http_session.py
@@ -3,7 +3,7 @@ from vjyscan.cli.prints import *
 
 
 class VJScan:
-    def __init__(self, ua, proxy, verbose):
+    def __init__(self, ua, proxy, cookie, verbose):
         self.print_vulnerable = print_vulnerable
         self.print_found = print_found
         if verbose:
@@ -15,6 +15,8 @@ class VJScan:
             self.print_not_vulnerable = dummy
             self.print_not_found = dummy
         self.http_client = requests.Session()
-        self.http_client.headers.update(ua)
+        self.http_client.headers.update({'User-Agent': ua})
         if proxy:
-            self.http_client.proxies.update(proxy)
+            self.http_client.proxies.update({'http': proxy})  # must check https proxies
+        if cookie:
+            self.http_client.cookies.set_cookie(cookie)
