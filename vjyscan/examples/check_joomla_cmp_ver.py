39c471602af5de273c400ee9eb3b16e217822b58
Nong Hoang Tu <dmknght@parrotsec.org>
2021-09-30 05:05:39 +0700

    Add more test for accurate comparison

--- vjyscan/examples/check_joomla_cmp_ver.py
+++ vjyscan/examples/check_joomla_cmp_ver.py
@@ -8,6 +8,9 @@ db_path = joomla.__path__[0] + "core.jdb" if joomla.__path__[0].endswith("/") el
 assert joomla_cmp("0.2.3", "0.2.3") == True, "Wrong, 0.2.3"
 assert joomla_cmp("2.3.7", "2.0.0<=2.5.7") == True, "Wrong, 2.0.0<=2.5.7"
 assert joomla_cmp("2.3.5", "1.1.1<=1.1.3") == False, "Wrong 1.1.1<=1.1.3"
+assert joomla_cmp("2.3.5", "2.0.0<2.3.5") == False, "Wrong 2.0.0<2.3.5"
+assert joomla_cmp("2.3.5", "2.0.0<=2.3.5") == True, "Wrong 2.0.0=<2.3.5"
+
 
 for line in open(db_path):
     vuln_info = json.loads(line)
