927e3802d065a358c87b0e64dcd01893b901d1b0
Nong Hoang Tu <dmknght@parrotsec.org>
2021-09-29 20:42:38 +0700

    Add test real site with vuln db

--- vjyscan/examples/parse_joomla_version.py
+++ vjyscan/examples/parse_joomla_version.py
@@ -1,9 +1,20 @@
 from vjyscan.cores import http_session
 from vjyscan.modules.fingerprints.joomla import version_parser
+from vjyscan.resources import joomla
+from vjyscan.cores.version_cmp import *
+import json
 
-session = http_session.VJScan(verbose=True, ua="Mozilla/5.0", cookie="", proxy="")
+db_path = joomla.__path__[0] + "core.jdb" if joomla.__path__[0].endswith("/") else joomla.__path__[0] + "/core.jdb"
+
+
+session = http_session.VJScan(verbose=False, ua="Mozilla/5.0", cookie="", proxy="")
 
 target_url = "https://ktht.nuce.edu.vn/"
 
 session.print_verbose(f"Checking {target_url}")
-print(version_parser.check_xml(session, target_url))
+version = version_parser.check_xml(session, target_url)
+
+for line in open(db_path):
+    vuln_info = json.loads(line)
+    if joomla_cmp("3.3.22", vuln_info["version"]):
+        session.print_vulnerable(f"{vuln_info['CVE']} {vuln_info['name']}")
