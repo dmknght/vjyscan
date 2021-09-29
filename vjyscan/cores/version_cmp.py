0764c1d897eeaf94fa716b7e1e2f8183a0e324d9
Nong Hoang Tu <dmknght@parrotsec.org>
2021-09-30 04:59:49 +0700

    Rewrite code structure for version cmp

--- vjyscan/cores/version_cmp.py
+++ vjyscan/cores/version_cmp.py
@@ -1,96 +1,111 @@
-def joomla_cmp(target_version: str, cmp_version: str) -> bool:
+"""
+Handle version cmp
+1. split multiple versions from db (could be ranges or just versions) to 1 versions
+2. Convert version length to same length (1.1.1 vs 2.2)
+3. Do comparison
+3a. Check exact match between target version and db version
+3b. Check exact match between target version and db range
+    - Check if target version >= min version
+    - Check if target version <= max version or target version < max version
+        Depends on db format
+"""
+
+import operator
+
+
+# def validate_versions(target_version, db_version: str):
+#     """
+#     Maybe the version could be in wrong format. Example: target version 1.2, db_version 1.2.3
+#     This function will convert format target_version and db_version to same range
+#     :param target_version: Target's version from parser. Format int_x.int_y.int_z (1.2.3, 1.1.1)
+#     :param db_version: Splited versions from db. Now it is single version is as same as db_version
+#     :return: target_version, db_version -> string but with same length
+#     """
+#     diff_dot = target_version.count(".") - db_version.count(".")
+#     if diff_dot > 0:
+#         # target_version: 1.1.1
+#         # db_version: 1.1
+#         for i in range(diff_dot):
+#             db_version += ".0"
+#     elif diff_dot < 0:
+#         # target_version: 1.1
+#         # db_version: 1.1.1
+#         for i in range(0 - diff_dot):
+#             target_version += ".0"
+#     return target_version, db_version
+
+
+def __do_cmp_version(cmp_operator, target_version, db_version) -> bool:
+    """
+
+    :param cmp_operator: operator callback
+    :param target_version:
+    :param db_version:
+    :return:
+    """
+    for target, db in zip(target_version.split("."), db_version.split(".")):
+        if not cmp_operator(target, db):
+            return False
+    return True
+
+
+def __cmp_with_db_version(target_version: str, db_version: str) -> bool:
+    """
+    Compare CMS's version with vulnerable versions in db. db_version have different syntax to define
+    vulnerable versions, so we use operator lib to do dynamic check
+    https://stackoverflow.com/a/36852350
+    :param target_version: format int_x.int_y.int_z
+    :param db_version: format int_x.int_y.int_z or int_x.int_y.int_z<=int_a.int_b.int_c
+        or int_x.int_y.int_z<int_a.int_b.int_c
+    :return: True if target_version is in the db_version range.
+    """
+    if "<=" in db_version:
+        do_op = operator.le
+        min_ver, max_ver = db_version.split("<=")
+        # THINK: do we need quick string check because we are doing cmp >= for min version
+        if target_version == min_ver or target_version == max_ver:
+            return True
+        if __do_cmp_version(operator.ge, target_version, min_ver) and \
+                __do_cmp_version(do_op, target_version, max_ver):
+            return True
+        else:
+            return False
+    elif "<" in db_version:
+        do_op = operator.le
+        min_ver, max_ver = db_version.split("<")
+        # THINK: do we need quick string check because we are doing cmp >= for min version
+        if target_version == min_ver:
+            return True
+        if __do_cmp_version(operator.ge, target_version, min_ver) and \
+                __do_cmp_version(do_op, target_version, max_ver) and \
+                max_ver != target_version:
+            return True
+        else:
+            return False
+    else:
+        if target_version == db_version:
+            return True
+        return False
+
+
+def joomla_cmp(target_version: str, db_versions: str) -> bool:
     """
     Original code: https://github.com/OWASP/joomscan/blob/master/core/compare.pl
     Original license: GPL-3
     Original version: 0.0.7 (release date Sep 24, 2018)
-    Py version: 0.0.1, Date 29th Sep 2021
-    :param target_version: Target's version from parser
-    :param cmp_version: Vulnerable version from DB, ex: 0.1.2<0.1.5 or 0.1.2
+    Py version: 0.0.2, Date 30th Sep 2021
+    Check if CMS version of website is in the range of vulnerable version. Each vulnerability in db
+    could have multiple versions / multiple version ranges/
+    :param target_version: Target's version from parser. ex: 1.2.3
+    :param db_versions: Vulnerable version from DB, ex: 0.1.2<0.1.5 or 0.1.2 or 1.2.3|4.5.6
+        or 1.2.3|4.5.6|7.7.7<7.8.8
     :return: bool -> True if target version is in vulnerable version group
     Example: joomla_cmp("0.2.3", "<=0.2.5")
     """
-    
-    def cmp(tv: list, start_v: list, end_v: list) -> bool:
-        """
-        :param tv: Target's version from parser
-        :param start_v: Vulnerable start version from DB
-        :param end_v: Vulnerable end version from DB
-        :return: bool -> True if target version is in vulnerable version group
-        """
 
-        for i in range(0, max(len(tv), len(start_v), len(end_v))):
-            if not tv[i]:
-                tv.append(0)
-
-            if not start_v[i]:
-                start_v.append(0)
-
-            if not end_v[i]:
-                end_v.append(0)
-
-            if int(start_v[i]) < int(tv[i]) < int(end_v[i]):
-                return True
-
-            if int(tv[i]) < int(start_v[i]) or int(tv[i]) > int(end_v[i]):
-                return False
-
-        return False
-
-    tversion = target_version.split(".")
-    cmp_version = cmp_version.replace('', '')
-    list_cmp_version = cmp_version.split('<')
-    
-    if len(list_cmp_version) >= 2:
-        start_version = list_cmp_version[0] if list_cmp_version[0] != '' else '0.0.0'
-        start_version = start_version if start_version != '=' else '0.0.0' + start_version
-        end_version = list_cmp_version[1]
-        
-        #ex: 1.2.4 compare 1.2.3=<=1.2.5 -> True
-        #ex: 1.2.3 compare 1.2.3=<=1.2.5 -> True
-        #ex: 1.2.5 compare 1.2.3=<=1.2.5 -> True
-        #ex: 1.2.2 compare 1.2.3=<=1.2.5 -> False
-        #ex:  1.2.6 compare 1.2.3=<=1.2.5 -> False
-        if start_version.endswith('=') and end_version.startswith('='):
-            
-            start_version = start_version.replace('=', '')
-            end_version = end_version.replace('=', '')
-            
-            if start_version == target_version or end_version == target_version:
-                return True
-            
-            start_version = start_version.split('.')
-            end_version = end_version.split('.')
-            
-            return cmp(tversion, start_version, end_version)
-        
-        #ex: 1.2.3 compare 1.2.3=<1.2.5 -> True
-        #ex: 1.2.5 compare 1.2.3=<1.2.5 -> False
-        if start_version.endswith('='):
-            start_version = start_version.replace('=', '')
-            
-            if start_version == target_version:
-                return True
-            
-            start_version = start_version.split('.')
-            end_version = end_version.split('.')
-            
-            return cmp(tversion, start_version, end_version)
-        
-        #ex: 1.2.3 compare 1.2.3<=1.2.5 -> False
-        #ex: 1.2.5 compare 1.2.3<=1.2.5 -> True
-        if end_version.startswith('='):
-            
-            end_version = end_version.replace('=', '')
-            
-            if end_version == target_version:
-                return True
-            
-            start_version = start_version.split('.')
-            end_version = end_version.split('.')
-            
-            return cmp(tversion, start_version, end_version)
-        
-    if len(list_cmp_version) == 1 and target_version == list_cmp_version[0]:
-        return True
-    
+    # Each CVE could have multiple versions / multiple version ranges. We compare with single range
+    # or single version first
+    for db_version in db_versions.split("|"):
+        if __cmp_with_db_version(target_version, db_version):
+            return True
     return False
