a58f82bd4c388313285d2b1fb376ed70259e27ac
Nong Hoang Tu <dmknght@parrotsec.org>
2021-09-30 04:59:39 +0700

    Improve error showing

--- vjyscan/examples/check_joomla_cmp_ver.py
+++ vjyscan/examples/check_joomla_cmp_ver.py
@@ -5,9 +5,9 @@ import json
 
 db_path = joomla.__path__[0] + "core.jdb" if joomla.__path__[0].endswith("/") else joomla.__path__[0] + "/core.jdb"
 
-assert joomla_cmp("0.2.3", "0.2.3") == True, "OK"
-assert joomla_cmp("2.3.7", "2.0.0<=2.5.7") == True, "OK"
-assert joomla_cmp("2.3.5", "1.1.1<=1.1.3") == False, "OK"
+assert joomla_cmp("0.2.3", "0.2.3") == True, "Wrong, 0.2.3"
+assert joomla_cmp("2.3.7", "2.0.0<=2.5.7") == True, "Wrong, 2.0.0<=2.5.7"
+assert joomla_cmp("2.3.5", "1.1.1<=1.1.3") == False, "Wrong 1.1.1<=1.1.3"
 
 for line in open(db_path):
     vuln_info = json.loads(line)
