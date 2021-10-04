4656bc433739ac4b27697ae1932ecf38e39d534a
Nong Hoang Tu <dmknght@parrotsec.org>
2021-10-04 07:15:11 +0700

    Add count vulns and add documentations

--- vjyscan/modules/enumerates/joomla/vuln_check.py
+++ vjyscan/modules/enumerates/joomla/vuln_check.py
@@ -5,9 +5,20 @@ from vjyscan.cores.version_cmp import *
 db_path = joomla.__path__[0] + "core.jdb" if joomla.__path__[0].endswith("/") else joomla.__path__[0] + "/core.jdb"
 
 
-def check_core_vulns(client, version: str):
+def check_core_vulns(client, version: str) -> int:
+    """
+    Compare version of Joomla with database
+    Return number of vulnerabilities
+    :param client: scan session to call print callbacks
+    :param version: Target's versions
+    :return:
+    """
+    count = 0
     for line in open(db_path):
         vuln_info = json.loads(line)
         if compare_versions(version, vuln_info["version"]):
+            count += 1
             client.print_verbose(f"Found {vuln_info['name']}. Comparison: {version} vs {vuln_info['version']}")
             client.print_vulnerable(vuln_info['name'], cve=vuln_info["CVE"])
+
+    return count
