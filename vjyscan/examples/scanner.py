82bc944cdc98b59ed2b77088c2dd2e3e980e7b89
Nong Hoang Tu <dmknght@parrotsec.org>
2021-10-06 19:30:12 +0700

    Call scan with new api

--- vjyscan/examples/scanner.py
+++ vjyscan/examples/scanner.py
@@ -1,17 +1,14 @@
 from vjyscan.cores import scan_session
-from vjyscan.modules.enumerates.joomla import version_parser, vuln_check
+from vjyscan.modules.enumerates.joomla import *
 from vjyscan.modules.fingerprints import commons
 
 target_url = "https://vjyscan.joomla.com/"
-session = scan_session.VJScan(verbose=False, ua="Mozilla/5.0", cookie="", proxy="")
+session = scan_session.VJScan(verbose=True, ua="Mozilla/5.0", cookie="", proxy="")
 
 session.print_info(f"Scanning {target_url}")
 resp = session.http_client.get(target_url)
 cms = commons.response_analysis(session, data=resp.text)
 if cms == "Joomla":
-    version = version_parser.check_xml(session, target_url)
-    if version:
-        session.print_info("Scanning vulnerabilities of core")
-        vuln_check.check_core_vulns(session, version)
+    version = handle_enumerate(session, target_url)
 
 print("Completed")
